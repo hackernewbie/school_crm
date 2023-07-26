f8de6d1831a7b173e53eddf78a01e574e76a893e
Rajiv Verma <rajiv@webguy.in>
2023-07-26 16:07:30 +0530

    Footer copyright update

---
+++ resources/views/backend/inc/footer.blade.php
@@ -0,0 +1,10 @@
+ <!--**********************************
+        Footer start
+        ***********************************-->
+ <div class="footer footer-outer">
+     <div class="copyright">
+         <p>© Powered by <a href="https://www.rajivverma.me" target="_blank">Kopou
+                 Web Solutions</a> 2023
+         </p>
+     </div>
+ </div>
---
+++ resources/views/backend/inc/preloader.blade.php
@@ -0,0 +1,18 @@
+<!--*******************
+        Preloader start
+    ********************-->
+<div id="preloader">
+    <div class="loader">
+        <div class="dots">
+            <div class="dot mainDot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+        </div>
+
+    </div>
+</div>
+<!--*******************
+        Preloader end
+    ********************-->
